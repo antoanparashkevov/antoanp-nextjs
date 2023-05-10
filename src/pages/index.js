7a2f4b7630c94a33e537938ebd8df7f32332bced
“antoanparashkevov” <tonkata1505@gmail.com>
2023-05-10 17:06:41 +0300

    navigate programatically

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,9 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
-export default function Home() {
+export default function HomePage () {
+    const router = useRouter();
+    
   return (
     <>
       <div>
@@ -27,6 +30,8 @@ export default function Home() {
             >
               Service 1</Link>
           </li>
+            <button onClick={() => router.push('/services/1')}>Navigate to Service 1</button>
+            <button onClick={() => router.push({ pathname: '/services/[serviceId]', query: { serviceId : '1' } } ) }>Navigate to Service 1 again</button>
         </ul>
       </div>
     </>
