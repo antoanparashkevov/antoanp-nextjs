32f9b608aaaa4b3e89927f6b59e21b8471863622
“antoanparashkevov” <tonkata1505@gmail.com>
2023-07-01 08:45:09 +0300

    add head tg

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,6 @@
+import { Fragment } from "react";
+import Head from 'next/head';
+
 import '@/styles/globals.scss'
 
 //layout component
@@ -5,8 +8,17 @@ import Layout from "@/components/layout/Layout";
 
 export default function App({ Component, pageProps }) {
   return (
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <Fragment>
+          <Head>
+              <title>Business Web Applications</title>
+              <meta 
+                  name='description' 
+                  content='Find out the best business solution for you by making a web application of your choise with the latest technologies'/>
+              <meta name='viewport'  content='initial-scale=1.0, width=device-width'/>
+          </Head>
+          <Layout>
+              <Component {...pageProps} />
+          </Layout>
+      </Fragment>
   )
 }
