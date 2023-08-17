293effd2ba6cd5425488352468a07c13e14aa3bb
antoanparashkevov <antoanparashkevov@gmail.com>
2023-08-17 22:28:46 +0300

    Create an API Route named applications, connect to the mongoDB client, post the data to the MongoDB database

--- src/pages/api/applications/[senderId].js
+++ src/pages/api/applications/[senderId].js
@@ -1,7 +1,6 @@
 
 export default function handler(req, res) {
     const senderId = req.query['senderId'];
-
     console.log('senderId >>> ', senderId)
 
     res.status(200).json({message: senderId})
---
+++ src/pages/api/applications/index.js
@@ -0,0 +1,40 @@
+import databaseConfiguration from "../../../../config/mongodb/database-configuration";
+
+import Application from "../../../../models/Application";
+
+/**
+ *
+ * @param {import('next').NextApiRequest} req
+ * @param {import('next').NextApiResponse} res
+ * */
+
+
+export default async function applications(req, res) {
+
+    await databaseConfiguration();
+
+    if( req.method === 'POST' ) {
+        //already parsed body
+        console.log('req.body >>> ', req.body)
+
+        const { email, budget, message } = req.body;
+
+        try {
+            const item = await Application.create({
+                email,
+                budget,
+                message
+            })
+            console.log('item that was created >>> ', item);
+            res.status(200).json({
+                message: 'Success!',
+                submittedData: item
+            })
+        } catch (error) {
+            res.status(400).json(error);
+        }
+    } else {
+        //401 - forbidden
+        res.status(401).json({ message: 'This API Route accepts only POST requests!'})
+    }
+}
