e95e90e78d83d67521b602e65e6dee71c333bd70
“antoanparashkevov” <tonkata1505@gmail.com>
2023-08-23 14:35:32 +0300

    Scroll to the form when click on Contact Button

--- src/components/home/AboutSection.js
+++ src/components/home/AboutSection.js
@@ -6,6 +6,23 @@ import CoverImage from '../../../public/images/cover_without_bg.jpg';
 import { FilledButton } from "@/components/UI/BaseButton";
 
 const AboutSection = () => {
+    
+    const HandleContactClick = () => {
+       const footer = document.getElementById('footer-container');
+        // console.log('document.body.scrollHeight', document.body.scrollHeight)
+        // console.log('footer.offsetHeight', footer.offsetHeight)
+        // console.log('document.body.scrollHeight - footer.offsetHeight', document.body.scrollHeight - footer.offsetHeight)
+        
+        let scrollPx = (document.body.scrollHeight - footer.offsetHeight) - document.body.clientHeight;
+       if( footer && document.body && document.body.scrollHeight ) {
+
+           window.scrollTo({
+               top: scrollPx,
+               left: 0,
+               behavior: "smooth"
+           })
+       }
+    }
 
     return (
         <section className={ styles['about_section'] }>
@@ -13,7 +30,7 @@ const AboutSection = () => {
                 <h1 className={styles['about_section_title']}>I have <span>Web Development</span> Experience</h1>
                 <p className={styles['about_section_desc']}>Hello everyone, I am Antoan! I have been helping businesses to solve their problems by making a web
                     application of their choice.</p>
-                <FilledButton className={styles['about_section_btn']}>Contact Me</FilledButton>
+                <FilledButton onClick={HandleContactClick} className={styles['about_section_btn']}>Contact Me</FilledButton>
             </div>
             <div className={ styles['about_section_image'] }>
                 <Image src={CoverImage} alt='Cover Image'/>
--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -3,7 +3,7 @@ import styles from './Footer.module.scss';
 const Footer = () => {
     
     return (
-        <div className={styles['footer_container']}>
+        <div className={styles['footer_container']} id='footer-container'>
             <span>@ 2023. All Rights Reserved</span>
             <div className={styles['footer_social_icons']}>
                 
