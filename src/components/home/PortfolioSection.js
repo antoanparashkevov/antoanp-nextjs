7392ab5027f1ae5423e8a5c6c9de0192af322a66
“antoanparashkevov” <tonkata1505@gmail.com>
2023-08-23 14:42:15 +0300

    Imperative approach for routing (Portfolio Page)

--- src/components/home/PortfolioSection.js
+++ src/components/home/PortfolioSection.js
@@ -1,16 +1,24 @@
 import styles from './PortfolioSection.module.scss';
-import { FilledButton } from "@/components/UI/BaseButton";
+import { useRouter } from 'next/router';
 
 //components
+import { FilledButton } from "@/components/UI/BaseButton";
 import PortfolioList from "@/components/home/PortfolioList";
 
 const PortfolioSection = () => {
+    const router = useRouter();
+    
     return (
         <section className={styles['portfolio_section']}>
             <div className={styles['portfolio_section_main_content']}>
                 <h1 className={styles['portfolio_section_title']}>My latest <span>Projects</span></h1>
                 <p className={styles['portfolio_section_desc']}>Here are some of my latest projects to share with you.</p>
-                <FilledButton className={styles['portfolio_section_btn']}>Show More</FilledButton>
+                <FilledButton 
+                    className={styles['portfolio_section_btn']}
+                    onClick={() => router.push('/portfolio')}
+                >
+                    Show More
+                </FilledButton>
             </div>  
             <div className={styles['portfolio_section_main_desc']}>
                 <PortfolioList />
