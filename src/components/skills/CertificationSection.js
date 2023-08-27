026e0c8d3ef28b554bde61be6b06d070d25d3d40
“antoanparashkevov” <tonkata1505@gmail.com>
2023-08-27 16:58:52 +0300

    Styles for Certification section + ListWrapper for certificates

--- src/components/skills/CertificationSection.js
+++ src/components/skills/CertificationSection.js
@@ -1,24 +1,39 @@
 import styles from './CertificationSection.module.scss';
 
+import ITCommunity from '../../../public/images/itcommunity.png'
+
+//components
+import ListWrapper from "@/components/home/ListWrapper";
+
 const CertificationSection = () => {
+    
+    const certificates = [
+        // TODO replace with real certificates
+        {
+            source: ITCommunity,
+            name: 'Google',
+            url: 'https://google.com'
+        }
+    ]
 
     return (
         <section className={ styles['certification_section'] }>
             <div className={ styles['certification_section_main_content'] }>
                 <h1 className={ styles['certification_section_title'] }>
-                    Solid JavaScript and Node JS knowledge gained from a Software Academy
+                    Solid <span>JavaScript</span> and <span>Node JS</span> knowledge gained from a Software Academy
                 </h1>
                 <p className={ styles['certification_section_desc'] }>
-                    I admit that I learned the most from the real environment, and not so much from the theory and the
+                    I admit that I learned the most from the <span>real environment</span>, and not so much from the
+                    theory and the
                     partial practices in the
-                    academy. When developing a real project for a client, you face many obstacles. Only theory cannot
+                    academy. When developing a <span>real project</span> for a client, you face many obstacles. Only theory cannot
                     help you to solve all problems. But without theory you will not know how to describe the problem and
-                    find a solution for it. You can see my certificates obtained during my studies at the academy on
+                    find a solution for it. You can see my <span>certificates</span> obtained during my studies at the <span>academy</span> on
                     this section.
                 </p>
             </div>
             <div className={ styles['certification_section_certifications_list'] }>
-
+                <ListWrapper listData={ certificates }/>
             </div>
         </section>
     )
