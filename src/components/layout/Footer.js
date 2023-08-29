b6beb7946aa8959a1e7e97ad71c0372612b8cdb3
“antoanparashkevov” <tonkata1505@gmail.com>
2023-08-29 19:04:40 +0300

    Footer react icons

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,12 +1,19 @@
 import styles from './Footer.module.scss';
 
+import { FaGithub, FaLinkedin, FaFigma } from 'react-icons/fa'
+
 const Footer = () => {
+    const anchorStyles = {
+        textDecoration: 'none',
+        color: 'inherit'
+    }
     
     return (
-        <div className={styles['footer_container']} id='footer-container'>
+            <div className={styles['footer_container']} id='footer-container'>
             <span>@ 2023. All Rights Reserved</span>
             <div className={styles['footer_social_icons']}>
-                
+                <a style={anchorStyles} href='https://github.com/antoanparashkevov'><FaGithub size={20} /></a>
+                <a style={anchorStyles} href="https://www.linkedin.com/in/antoanp"><FaLinkedin size={20} /></a>
             </div>
         </div>
     )
