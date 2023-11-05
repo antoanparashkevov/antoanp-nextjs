9055df17707de8308811337afe66eec098499e84
“antoanparashkevov” <tonkata1505@gmail.com>
2023-11-05 13:26:32 +0200

    Modify the icon component

--- src/components/UI/Icon.js
+++ src/components/UI/Icon.js
@@ -4,16 +4,14 @@ import styles from './Icon.module.scss';
 const Icon = ({iconSrc, iconAlt, iconWidth, iconHeight, placeholder = 'empty', fill = false}) => {
     return (
         <div className={styles['icon_container']}>
-            <picture className={styles['icon_picture']}>
-                <Image 
-                    src={iconSrc} 
-                    alt={iconAlt} 
-                    width={iconWidth} 
-                    height={iconHeight} 
-                    placeholder={placeholder}
-                    fill={fill} 
-                />
-            </picture>
+            <Image 
+                src={iconSrc} 
+                alt={iconAlt} 
+                width={iconWidth} 
+                height={iconHeight} 
+                placeholder={placeholder}
+                fill={fill} 
+            />
         </div>
     )
 }
