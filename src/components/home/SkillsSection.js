525882e535045bea704670bc0e4513cb1a6e9b6b
“antoanparashkevov” <tonkata1505@gmail.com>
2023-11-05 16:46:35 +0200

    Modify the skills section

--- src/components/home/SkillsSection.js
+++ src/components/home/SkillsSection.js
@@ -1,39 +1,115 @@
+import React, { Fragment } from "react";
 import styles from './SkillsSection.module.scss';
-
-
 
 //static text
 import { skillParts } from "@/util/data/skillParts";
 
+//static images
+import MoneyBack from '../../../public/images/money_back.png';
+
 //UI components
 import Icon from "@/components/UI/Icon";
 import Badge from "@/components/UI/Badge";
+import Image from "next/image";
 
 const SkillsSection = () => {
 
     return (
         <section className={ styles['skills_section'] }>
+            <strong className={ styles['skills_section_main_intro'] }>
+                Besides building your website from SCRATCH, you will also get:
+            </strong>
             <div className={ styles['skills_section_grid'] }>
                 {
                     skillParts.map(skillPart => {
                         return (
-                            <article key={skillPart.name} className={ styles['skills_section_grid_item'] }>
-                                <div className={styles['skills_section_grid_item_icon']}>
+                            <article key={ skillPart.name } className={ styles['skills_section_grid_item'] }>
+                                <div className={ styles['skills_section_grid_item_icon'] }>
                                     <Icon
-                                        iconSrc={skillPart.iconSrc}
-                                        iconAlt={skillPart.name}
-                                        iconWidth={30}
-                                        iconHeight={30}
+                                        iconSrc={ skillPart.iconSrc }
+                                        iconAlt={ skillPart.name }
+                                        iconWidth={ 28 }
+                                        iconHeight={ 28 }
                                     />
                                 </div>
-                                <header className={ styles['skills_section_grid_item_header'] }>{ skillPart.name }</header>
+                                <header className={ styles['skills_section_grid_item_header'] }>
+                                    { skillPart.name.map((element, index, nameArray) => {
+                                        return (
+                                            <Fragment key={ index }>
+                                                { index % 2 === 0 ?
+                                                    nameArray[index] :
+                                                    <Fragment>
+                                                        &nbsp;
+                                                        { React.createElement('span', null, nameArray[index]) }
+                                                        &nbsp;
+                                                    </Fragment>
+                                                }
+                                            </Fragment>
+                                        )
+                                    }) }
+                                </header>
                                 <p className={ styles['skills_section_grid_item_para'] }>
-                                    {skillPart.description}
+                                    { skillPart.description.map((element, index, descriptionArray) => {
+                                        return (
+                                            <Fragment key={ index }>
+                                                { index % 2 === 0 ?
+                                                    descriptionArray[index] :
+                                                    <Fragment>
+                                                        &nbsp;
+                                                        { React.createElement('span', null, descriptionArray[index]) }
+                                                        &nbsp;
+                                                    </Fragment>
+                                                }
+                                            </Fragment>
+                                        )
+                                    }) }
                                 </p>
                             </article>
                         )
                     })
                 }
+            </div>
+            <div className={ styles['skills_section_main_desc'] }>
+                <h1 className={ styles['skills_section_main_desc_title'] }><strong>But WAIT, that’s not
+                    everything!</strong></h1>
+                <p className={ styles['skills_section_main_desc_para'] }>If you sign up for a call with me over the next
+                    24 hours you will also get <span>2 SPECIAL BONUSES</span> in addition to your website:</p>
+                <ul>
+                    <li className={ styles['skills_section_main_desc_list_item'] }>
+                        <strong>
+                            Bonus 1: <span>SEO Optimization</span> or <span>SEO</span> in short
+                        </strong>
+                        <p>
+                            - This is a <span>unique</span> service that will improve the visibility and ranking of your
+                            website. The
+                            primary goal of SEO optimization is to <span>increase organic (non-paid) traffic</span> to
+                            your website
+                            by making it more appealing to search engines like Google, Bing, and Yahoo.
+                        </p>
+                    </li>
+                    <li className={ styles['skills_section_main_desc_list_item'] }>
+                        <strong>
+                            Bonus 2: <span>Responsive Design</span> - [ short explanation of the service]
+                        </strong>
+                        <p>
+                            - This <span>special</span> service will make sure your customers can look at your
+                            website <span>BOTH</span> on a <span>desktop</span> or <span>mobile</span> version.
+                        </p>
+                    </li>
+                </ul>
+                <div className={ styles['skills_section_main_outro'] }>
+                    <Image
+                        src={ MoneyBack }
+                        alt='Money Back'
+                        width={ 250 }
+                        height={ 250 }
+                        placeholder='blur'
+                    />
+                    <p>
+                        And of course, If for some reason you are not satisfied with ANY of my services, you will get a&nbsp;
+                        <strong>100% refund</strong> of your money in the blink of an eye.
+                    </p>
+                </div>
             </div>
         </section>
     )
--- src/util/data/skillParts.js
+++ src/util/data/skillParts.js
@@ -4,33 +4,118 @@ import BackEndIcon from '../../../public/icons/pulse-outline.svg';
 
 export const skillParts = [
     {
-        name : 'Front End Part',
-        description : 'Think of this service as the artistry of your website. It\'s where I craft the visual charm and user-friendly features that\'ll keep your visitors coming back for more.',
+        name : [ 'Front End', 'Part' ],
+        description : [
+            'Think of this service as the',
+            'artistry',
+            'of your website. It\'s where I craft the',
+            'visual charm',
+            'and',
+            'user-friendly',
+            'features that\'ll keep your visitors coming back for more.'
+        ],
         iconSrc : FrontEndIcon
     },
     {
-        name : 'Back End Part',
-        description : 'Ever wondered what makes your favorite websites and apps tick? That\'s where backend development swoops in.',
+        name : [ 'Back End', 'Part' ],
+        description : [
+            'Want to peek behind the curtain of your website? That\'s where the',
+            'back-end part',
+            'comes in. It\'s the',
+            'powerhouse',
+            'that ensures everything runs',
+            'smoothly and securely',
+            '. From databases to application logic, this service is the', 'backbone element', 'of your website\'s success.'
+        ],
         iconSrc : BackEndIcon
     },
     {
-        name : 'User functionalities - Where interaction meets fun',
-        description : 'I’ll make your website a user\'s paradise. User functionalities are the cool features that keep visitors engaged. From sign-up forms to interactive tools, we create the magic that makes your website a fun place to be.',
+        name : [ 'User functionalities' ],
+        description : [
+            'I’ll make your website', 'a user\'s paradise',
+            '. User functionalities are the',
+            'cool features',
+            'that keep visitors engaged. From sign-up forms to interactive tools, we create the',
+            'magic',
+            'that makes your website a fun place to be.'
+        ],
         iconSrc : BackEndIcon
     },
     {
-        name : 'Validation - Error proof decision',
-        description : 'Don\'t you hate it when forms fail or links lead nowhere? The validation service makes sure your website runs like a Swiss watch. No more errors; it\'s all about precision and reliability.',
+        name : [ 'Validation' ],
+        description : [
+            'Don\'t you hate it when forms fail or links lead nowhere? The validation service makes sure your website',
+            'runs like a Swiss watch',
+            '. No more errors; it\'s all about',
+            'precision',
+            'and',
+            'reliability.'
+        ],
         iconSrc : BackEndIcon
     },
     {
-        name : 'User Experience (UX) - Crafted with You in mind',
-        description : 'Welcome to the heart of your website\'s experience! UX is where I put on your user-centric thinking caps. I’ll create a website that\'s a breeze to navigate, ensuring every click, scroll, and interaction is a joy.',
+        name : [ 'User Experience (', 'UX', ')' ],
+        description : [
+            'Welcome to the',
+            'heart', 'of your website\'s experience! UX is where I put on your user-centric thinking caps. I’ll create a website that\'s a',
+            '"breeze"',
+            'to navigate, ensuring',
+            'every click, scroll',
+            ', and',
+            'interaction',
+            'is a joy.'
+        ],
         iconSrc : BackEndIcon
     },
     {
-        name : 'User Interface (UI) - Where beauty meets function',
-        description : 'It\'s time to make your website not only stunning but incredibly easy to use. UI is our canvas, and we paint it with colors, fonts, and layouts that not only look fantastic but also guide users effortlessly.',
+        name : [ 'User Interface (', 'UI', ')' ],
+        description : [
+            'It\'s time to make your website not only',
+            'stunning',
+            'but incredibly',
+            'easy to use',
+            '. UI is our canvas, and we paint it with',
+            'colors, fonts, and layouts',
+            'that not only look fantastic but also guide users effortlessly.'
+        ],
+        iconSrc : BackEndIcon
+    },
+    {
+        name : [ 'Accessibility' ],
+        description : [
+            'We\'re all about opening digital doors to',
+            'everyone.',
+            'Accessibility is our commitment to ensuring that',
+            'every visitor, regardless of ability,',
+            'can enjoy your website. It\'s about making the web an inclusive place for all.',
+        ],
+        iconSrc : BackEndIcon
+    },
+    {
+        name : [ 'Responsive design' ],
+        description : [
+            'In the digital age, your website needs to be a',
+            'chameleon.',
+            'That\'s where responsive design',
+            'shines.',
+            'We ensure your website looks great on',
+            'desktops, tablets, and mobiles,',
+            'giving users a',
+            'fantastic experience',
+            'no matter where they are.',
+        ],
+        iconSrc : BackEndIcon
+    },
+    {
+        name : [ 'Search Engine Optimization'],
+        description : [
+            'Want to shine in the vast web galaxy?',
+            'Our SEO service is your',
+            'guiding star.',
+            'We ensure your website',
+            'shows up at the top of search results. It\'s like having a spotlight on your website,',
+            'attracting more visitors.',
+        ],
         iconSrc : BackEndIcon
     }
 ]
