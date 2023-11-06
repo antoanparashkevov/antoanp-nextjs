b6d10ed96271662ca39b63b2332b12fe2d9c2130
“antoanparashkevov” <tonkata1505@gmail.com>
2023-11-06 17:05:00 +0200

    Modify the work experience description

--- src/components/about/WorkExperience.js
+++ src/components/about/WorkExperience.js
@@ -10,15 +10,10 @@ const WorkExperience = () => {
                 <h1 className={ styles['work_experience_title'] }>Currently working as
                     a <span>Software Engineer</span> in a sports company</h1>
                 <p className={ styles['work_experience_desc'] }>
-                    I’m working in a sports company called <span>Enetpulse</span> for <span>2+ years</span> now as a
+                    I’m working in a sports company called Enetpulse for <span>2+ years</span> now as a
                     Software Engineer where I have
-                    one of the <span>most crucial and responsible</span> tasks which is managing over <span>40 different widgets</span> for variety types of sports.
-                    You can think of а widget as a part of a website that displays statistics
-                    for a variety of sports. These widgets are used by <span>millions of users worldwide</span> and this
-                    makes my job
-                    so responsible. Thanks to my diligence and finesse to detail, I
-                    received <span>very good feedback</span> from my
-                    <span>coworkers</span> and my <span>boss</span>.
+                    one of the <span>most crucial and responsible</span> tasks which is managing <span>over 40 different widgets</span> for variety types of sports. 
+                    These widgets are used by <span>millions of users worldwide</span> and <span>a even single mistake</span> can be crucial.
                 </p>
             </div>
             <div className={ styles['work_experience_image'] }>
