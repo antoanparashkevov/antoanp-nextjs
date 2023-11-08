02e069b36fc5cd0dcf071f7b118e4d17a5bb2b63
“antoanparashkevov” <tonkata1505@gmail.com>
2023-11-08 18:44:45 +0200

    Bigger size for footer icons

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -7,8 +7,8 @@ const Footer = () => {
         <div className={ styles['footer_container'] } id='footer-container'>
             <span>@ 2023. All Rights Reserved</span>
             <div className={ styles['footer_social_icons'] }>
-                <a href='https://github.com/antoanparashkevov'><FaGithub size={ 20 }/></a>
-                <a href="https://www.linkedin.com/in/antoanp"><FaLinkedin size={ 20 }/></a>
+                <a href='https://github.com/antoanparashkevov'><FaGithub size={ 48 }/></a>
+                <a href="https://www.linkedin.com/in/antoanp"><FaLinkedin size={ 48 }/></a>
             </div>
         </div>
     )
