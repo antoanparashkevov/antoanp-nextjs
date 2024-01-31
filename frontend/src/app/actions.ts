df861d6df67b1a6dda0308eb25d0a3f0680d9f30
“antoanparashkevov” <tonkata1505@gmail.com>
2024-01-31 22:43:02 +0200

    Enhance the create Server action and validate the form data, send back either an error or a success message

--- frontend/src/app/actions.ts
+++ frontend/src/app/actions.ts
@@ -1,8 +1,43 @@
-'use server';//module level Server Actions
+"use server"; //module level Server Actions defined by 'use server' React directive
 
-//Server Action
-export async function create(activeTicketCode: string, formData: FormData) {
+import { z } from "zod";
 
-	console.log("invoked");
-	console.log("formData", formData);
+const schema = z.object({
+	firstName: z
+		.string({
+			invalid_type_error:
+				"Invalid First Name. Should be at least 2 characters long."
+		})
+		.min(2),
+	lastName: z
+		.string({
+			invalid_type_error:
+				"Invalid Last Name. Should be at least 2 characters long."
+		})
+		.min(2),
+	email: z.string({
+		invalid_type_error: "Invalid Email"
+	})
+});
+
+//Server Action - async functions that are executed on the server
+export async function create(ticketCode: string, initialState: any, formData: FormData) {
+	const validateFields = schema.safeParse({
+		firstName: formData.get("firstName"),
+		lastName: formData.get("lastName"),
+		email: formData.get("email")
+	});
+
+	if ( validateFields.success === false ) {
+
+		return {
+			errors: validateFields.error.flatten().fieldErrors,
+			message: ""
+		};
+	}
+
+	return {
+		errors: null,
+		message: "You've successfully sent the message!"
+	};
 }
