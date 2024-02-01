18f8b1680c499e1f40e23fb850e136fad7f8ba5e
“antoanparashkevov” <tonkata1505@gmail.com>
2024-02-01 13:14:54 +0200

    Update the server action by establishing a connection to the db and save a record when submitting the form

--- frontend/src/app/actions.ts
+++ frontend/src/app/actions.ts
@@ -2,39 +2,55 @@
 
 import { z } from "zod";
 
+import mongoDBClient from "../../mongoose/config/configuration";
+import { applicationItem, create as createApplication } from "../../mongoose/services/applicationService";
+
 const schema = z.object({
 	firstName: z
-		.string({
-			invalid_type_error:
-				"Invalid First Name. Should be at least 2 characters long."
-		})
-		.min(2),
+		.string()
+		.trim()
+		.min(2, { message: "Your first name should contain at least 2 characters!"}),
 	lastName: z
-		.string({
-			invalid_type_error:
-				"Invalid Last Name. Should be at least 2 characters long."
-		})
-		.min(2),
-	email: z.string({
-		invalid_type_error: "Invalid Email"
-	})
+		.string()
+		.trim()
+		.min(2, { message: "Your last name should contain at least 2 characters!"}),
+	email: z
+		.string()
+		.trim()
+		.email({ message: "Invalid email address"}),
+	ticketCode: z
+		.string()
+		.trim()
+		.min(4, { message: "Something went wrong with selecting a plan! Please, reload the page and try again!" })
 });
 
 //Server Action - async functions that are executed on the server
 export async function create(ticketCode: string, initialState: any, formData: FormData) {
+	const fields: applicationItem = {
+		firstName: formData.get("firstName") as string || '',
+		lastName: formData.get("lastName") as string || '',
+		email: formData.get("email") as string || '',
+		message: formData.get("message") as string || '',
+		ticketCode
+	}
+
 	const validateFields = schema.safeParse({
-		firstName: formData.get("firstName"),
-		lastName: formData.get("lastName"),
-		email: formData.get("email")
+		firstName: fields.firstName,
+		lastName: fields.lastName,
+		email: fields.email,
+		ticketCode: fields.ticketCode
 	});
 
 	if ( validateFields.success === false ) {
-
 		return {
-			errors: validateFields.error.flatten().fieldErrors,
+			errors: validateFields.error.issues,
 			message: ""
 		};
 	}
+
+	await mongoDBClient(); //establish a connection to the MongoDB
+
+	await createApplication(fields);//create a record in the DB
 
 	return {
 		errors: null,
