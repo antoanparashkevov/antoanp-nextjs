a26cc003c0cffbae78d83242ae85969bc12e86d3
“antoanparashkevov” <tonkata1505@gmail.com>
2024-02-11 11:00:23 +0200

    Create login Server action

--- frontend/src/lib/actions.ts
+++ frontend/src/lib/actions.ts
@@ -3,37 +3,48 @@
 import { z } from "zod";
 
 import mongoDBClient from "../../mongoose/config/configuration";
-import { applicationItem, create as createApplication } from "../../mongoose/services/applicationService";
+
+import {
+	applicationItem,
+	create as createApplication
+} from "../../mongoose/services/applicationService";
+
+import {
+	userItem,
+	login as loginUser,
+	register as registerUser
+} from "../../mongoose/services/authService";
+import { signIn } from "./auth";
 
 const schema = z.object({
-	firstName: z
-		.string()
-		.trim()
-		.min(2, { message: "Your first name should contain at least 2 characters!"}),
-	lastName: z
-		.string()
-		.trim()
-		.min(2, { message: "Your last name should contain at least 2 characters!"}),
-	email: z
-		.string()
-		.trim()
-		.email({ message: "Invalid email address"}),
-	ticketCode: z
-		.string()
-		.trim()
-		.min(4, { message: "Something went wrong with selecting a plan! Please, reload the page and try again!" })
+	firstName: z.string().trim().min(2, {
+		message: "Your first name should contain at least 2 characters!"
+	}),
+	lastName: z.string().trim().min(2, {
+		message: "Your last name should contain at least 2 characters!"
+	}),
+	email: z.string().trim().email({ message: "Invalid email address" }),
+	ticketCode: z.string().trim().min(4, {
+		message:
+			"Something went wrong with selecting a plan! Please, reload the page and try again!"
+	})
 });
 
 //Server Action - async functions that are executed on the server
-export async function create(ticketCode: string, isExpired: boolean, initialState: any, formData: FormData) {
+export async function create(
+	ticketCode: string,
+	isExpired: boolean,
+	initialState: any,
+	formData: FormData
+) {
 	const fields: applicationItem = {
-		firstName: formData.get("firstName") as string || '',
-		lastName: formData.get("lastName") as string || '',
-		email: formData.get("email") as string || '',
-		message: formData.get("message") as string || '',
+		firstName: (formData.get("firstName") as string) || "",
+		lastName: (formData.get("lastName") as string) || "",
+		email: (formData.get("email") as string) || "",
+		message: (formData.get("message") as string) || "",
 		ticketCode,
 		isExpired
-	}
+	};
 
 	const validateFields = schema.safeParse({
 		firstName: fields.firstName,
@@ -42,7 +53,7 @@ export async function create(ticketCode: string, isExpired: boolean, initialStat
 		ticketCode: fields.ticketCode
 	});
 
-	if ( validateFields.success === false ) {
+	if (validateFields.success === false) {
 		return {
 			errors: validateFields.error.issues,
 			message: ""
@@ -51,10 +62,67 @@ export async function create(ticketCode: string, isExpired: boolean, initialStat
 
 	await mongoDBClient(); //establish a connection to the MongoDB
 
-	await createApplication(fields);//create a record in the DB
+	await createApplication(fields); //create a record in the DB
 
 	return {
 		errors: null,
 		message: "You've successfully sent the message!"
+	};
+}
+
+const loginSchema = z.object({
+	email: z.string().trim().email({ message: "Invalid email address!" }),
+	password: z
+		.string()
+		.trim()
+		.min(4, { message: "You've provided an invalid password!" })
+});
+
+export async function login(initialState: any, formData: FormData) {
+	const fields: userItem = {
+		email: (formData.get("email") as string) || "",
+		password: (formData.get("password") as string) || ""
+	};
+
+	const validateFields = loginSchema.safeParse({
+		email: fields.email,
+		password: fields.password
+	});
+
+	if (validateFields.success === false) {
+		return {
+			errors: validateFields.error.issues,
+			message: ""
+		};
+	}
+
+	try {
+		await mongoDBClient(); //establish a connection to the MongoDB
+
+		const response = await loginUser(fields.email, fields.password); //login
+
+		if( response.message !== 'Passed' ) {
+			throw new Error("Something went wrong!");
+		}
+		
+		await signIn(fields.email);
+
+	} catch (error) {
+		if( error instanceof Error ) {
+			return {
+				errors: error.message || "Something went wrong!",
+				message: ""
+			};
+		}
+
+		return {
+			errors: "Something went wrong!",
+			message: ""
+		};
+	}
+
+	return {
+		errors: null,
+		message: "You've successfully logged in!"
 	};
 }
