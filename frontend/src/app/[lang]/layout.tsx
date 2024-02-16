02829d8e01c1027781e06c992c51d88cb9e1cfd5
“antoanparashkevov” <tonkata1505@gmail.com>
2024-02-16 19:39:30 +0200

    Wrap the root layout with the language provider

--- frontend/src/app/[lang]/layout.tsx
+++ frontend/src/app/[lang]/layout.tsx
@@ -7,6 +7,7 @@ import '@/styles/globals.css';
 import TheHeader from '@/components/layout/TheHeader';
 
 import { headerContent, metadataContent } from '@/lib/content';
+import { LanguageContextProvider } from '@/context/language-context';
 
 const source_sans_3 = Source_Sans_3({
 	weight: ['400', '600', '700'],
@@ -36,19 +37,21 @@ export default function RootLayout({
 
 	return (
 		<html lang={params.lang ?? defaultLocale} className={`${source_sans_3.variable} font-sans`}>
-			<body className="min-w-[360px] w-full min-h-screen flex flex-col text-main text-base">
-				<div id='overlay' />
-				<div id='backdrop' />
-				<header className="container flex w-full min-h-[130px] mb-16">
-					<TheHeader content={headerContent[lang]} />
-				</header>
-				<main className="container flex flex-col flex-grow justify-start items-center">
-					{children}
-				</main>
-				<footer className="min-h-[90px] flex items-end">
-					<span className="block w-full h-1 bg-blue-600"></span>
-				</footer>
-			</body>
+			<LanguageContextProvider params={params}>
+				<body className="min-w-[360px] w-full min-h-screen flex flex-col text-main text-base">
+					<div id='overlay' />
+					<div id='backdrop' />
+					<header className="container flex w-full min-h-[130px] mb-16">
+						<TheHeader content={headerContent[lang]} />
+					</header>
+					<main className="container flex flex-col flex-grow justify-start items-center">
+						{children}
+					</main>
+					<footer className="min-h-[90px] flex items-end">
+						<span className="block w-full h-1 bg-blue-600"></span>
+					</footer>
+				</body>
+			</LanguageContextProvider>
 		</html>
 	);
 }
