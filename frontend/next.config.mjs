fb3dc9b22a9c39b323d77d257d7db2b6a53c3cf9
antoanparashkevov <antoanparashkevov@gmail.com>
2024-03-15 17:15:15 +0200

    Add dockerignore, docker-compose build file for production, Dockerfile

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: "standalone",
     reactStrictMode: true,//by default, it is true. This is a dev mode only feature for highlighting potential problems in an app
 };
 
