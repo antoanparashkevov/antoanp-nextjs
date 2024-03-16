0cf17a55562661b735a44e986e5fa4ca881ebc7b
antoanparashkevov <antoanparashkevov@gmail.com>
2024-03-16 18:05:17 +0200

    Add gzip, caching

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "standalone",
+    compress: false,
     reactStrictMode: true,//by default, it is true. This is a dev mode only feature for highlighting potential problems in an app
 };
 
