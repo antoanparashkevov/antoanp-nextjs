5b12e46750ab4081f53baff01d683da11310558c
“antoanparashkevov” <tonkata1505@gmail.com>
2024-03-17 18:11:39 +0200

    Fix the Accept-Language: * issue

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -8,17 +8,20 @@ export const defaultLocale = 'en';
 function getLocale(request: NextRequest): string {
     const headers = new Headers(request.headers);
     const acceptLanguage = headers.get("Accept-Language");
-    console.log("acceptLanguage", acceptLanguage)
+
     if( acceptLanguage ) {
         headers.set("Accept-Language", acceptLanguage.replaceAll("_", "-"));
     }
 
     const headersObject = Object.fromEntries(headers.entries());
-    console.log("headersObject", headersObject)
+
     const languages = new Negotiator({ headers: headersObject }).languages();
-    console.log("languages", languages)
-    console.log("match(languages, supportedLocales, defaultLocale)", match(languages, supportedLocales, defaultLocale))
-    return match(languages, supportedLocales, defaultLocale);
+
+    if( languages.includes("*") === false ) {
+        return match(languages, supportedLocales, defaultLocale);
+    }
+
+    return match(["en"], supportedLocales, defaultLocale);
 }
 
 export function middleware(request: NextRequest) {
